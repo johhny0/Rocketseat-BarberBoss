aab9468565e4bc747e30460f54a34682c7b9e0f0
Johhny <jhasse20@hotmail.com>
2025-02-01 23:05:15 -0300

    autorization on API

--- RocketseatBarberBoss/Program.cs
+++ RocketseatBarberBoss/Program.cs
@@ -3,6 +3,9 @@ using Api.Middleware;
 using Application;
 using Infraestructure;
 using Infraestructure.Migrations;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,6 +16,8 @@ builder.Services.AddMvc(options => options.Filters.Add<ExceptionFilter>());
 
 builder.Services.AddApplicationServices();
 builder.Services.AddInfraServices(builder.Configuration);
+
+AddJwtBerar();
 
 var app = builder.Build();
 
@@ -25,6 +30,7 @@ app.UseMiddleware<CultureMiddleware>();
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
@@ -39,4 +45,29 @@ async Task MigrateDatabase()
     await using var scope = app.Services.CreateAsyncScope();
 
     await DataBaseMigration.MigrateDataBase(scope.ServiceProvider);
+}
+
+void AddJwtBerar()
+{
+
+    var sigingKey = builder.Configuration.GetValue<string>("Settings:Jwt:SigningKey")!;
+    var symmetricSecurityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(sigingKey));
+    var tokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = false,
+        ValidateAudience = false,
+        ClockSkew = new TimeSpan(0),
+        IssuerSigningKey = symmetricSecurityKey
+    };
+
+    builder.Services
+        .AddAuthentication(config =>
+    {
+        config.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        config.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    })
+        .AddJwtBearer(config =>
+    {
+        config.TokenValidationParameters = tokenValidationParameters;
+    });
 }
